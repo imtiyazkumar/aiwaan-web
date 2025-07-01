cc241faf8c30504131211ebd4d05ebbc42ab2bca
imtiyaz <imtiyazah907@gmail.com>
2025-07-01 23:31:16 +0530

    update

--- src/hooks/SessionHooks.tsx
+++ src/hooks/SessionHooks.tsx
@@ -5,7 +5,6 @@ import config from "../../config";
 
 export const ValidateSession = () => {
     const auth = useAuth();
-
     useIdleTimer({
         timeout: 1000 * 60 * 60 * config.inactivityDuration,
         promptTimeout: 0,
