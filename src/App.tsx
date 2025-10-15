1da25c1ce8649ef9be0b7d89eafe2517c5de6601
imtiyaz <imtiyazah907@gmail.com>
2025-10-15 23:48:17 +0530

    router upgrade

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-import router from "./routes/router";
-
-const App: React.FC = () => (
-    <RouterProvider router={router} />
-);
-
-export default App;
